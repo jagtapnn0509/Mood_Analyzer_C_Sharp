901ba105939eb2ef6265d8861e6c2ad91445a62e
jagtapnn0509 <jagtapnn05@gmail.com>
2021-05-12 10:49:22 +0530

    Add : function to Analyze the mood using Exception Handling

---
+++ UnitTest1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using MoodAnalyser;
+using MoodAnalyser1;
+using System;
+
+namespace UnitTest1MSTestWith.NetFreamWork
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject()
+        {
+            string message = null;
+            object expected = new MoodAnalyser(message);
+            object obj = MoodAnalyserFactory.CreateMoodAnalyser("MoodAnalyserAppWithCore.MoodAnalyser", "MoodAnalyser");
+            expected.Equals(obj);
+            //Assert.AreEqual(expected,obj);
+
+        }
+        [TestMethod]
+        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject_UsingParameterizedConstructor()
+        {
+            object expected = new MoodAnalyser("HAPPY");
+            object obj = MoodAnalyserFactory.CreateMoodAnalyseUsingParameterizedConstructor("MoodAnalyserAppWithCore.MoodAnalyser", "MoodAnalyser", "SAD");
+            expected.Equals(obj);
+        }
+    }
+
+}
